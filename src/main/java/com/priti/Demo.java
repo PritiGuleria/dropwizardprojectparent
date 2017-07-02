97ef15fff8cf1f38484ce636846eb57b9c8fb72a
PritiGuleria <pritiguleria10@gmail.com>
2017-07-02 19:56:28 +0530

    First commit

---
+++ src/main/java/com/priti/Demo.java
@@ -0,0 +1,11 @@
+package com.priti;
+
+/**
+ * Created by prguleri on 7/2/2017.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Kya main such me pagal hun ?? Bolo bolo bolo na");
+    }
+}
